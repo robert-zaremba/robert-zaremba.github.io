297f1ceef22b11688a66a38818d62622d788d8b4
Robert Zaremba <robert.zaremba@zoho.com>
2013-12-22 12:29:09 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,11 +10,11 @@ FOLDER=../robert-zaremba.github.com.deploy
 echo "deploy temp folder: $FOLDER"
 
 if [ -d "$FOLDER" ]; then
-	{ echo "ERROR: directory  $FOLDER  exists"; exit 1; }
+	echo "ERROR: directory  $FOLDER  exists"; exit 1;
 fi
-mv blog/html $FOLDER
+mv blog/html $FOLDER || { echo "can't move blog to $FOLDER"; exit 2; }
 rm -rf blog
-git co master
+git co master  || { echo "can't checkout to master"; exit 3; }
 mv .git*  $FOLDER/
 mv CNAME  $FOLDER/
 mv params.json  $FOLDER/
