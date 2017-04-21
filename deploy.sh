41bc5dfc61760b2268ae157645ca3e4b1a885867
Robert Zaremba <robert.zaremba@scale-it.pl>
2017-04-21 13:01:59 +0200

    added amend and message to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,40 @@ read
 #     exit 1;
 # fi
 
+syntax="Syntax: '[-amend] ['commit message']"
+if [[ $1 == *"-h"* ]]; then
+	echo $syntax; exit 100
+fi
+
+syntax="Wrong arguments. $syntax"
+amend=false
+if [[ $1 == *"-amend"* ]]; then
+	echo "AMENDING"
+	amend=true
+fi
+if $amend; then
+	case $# in
+		1) ;;
+		2) msg="${@: -1}";;
+		*) echo $syntax; exit 10;;
+	esac;
+else
+	case $# in
+		0) ;;
+		1) msg="${@: -1}";;
+		*) echo $syntax; exit 10;;
+	esac;
+fi
+
+
+
 git add -A
-git ci -a || exit 1
+if $amend; then
+	if [[ -z $msg ]]; then git ci -a --amend || exit 1
+	else git ci -a --amend -m $msg || exit 1
+	fi
+else git ci -a || exit 1
+fi
 
 FOLDER=../robert-zaremba.github.com.deploy
 echo "deploy temp folder: $FOLDER"
@@ -30,4 +62,10 @@ mv $FOLDER/* ./
 mv $FOLDER/.* ./
 rmdir $FOLDER
 git add -A
-git ci -a
+git add -A
+if $amend; then
+	if [[ -z $msg ]]; then git ci -a --amend || exit 1
+	else git ci -a --amend -m $msg || exit 1
+	fi
+else git ci -a || exit 1
+fi
